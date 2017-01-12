084e9dfaeafae0d640eea9a7541de6c924100dc1
hanzichi <hanzichi@outlook.com>
2017-01-12 11:19:39 +0800

    call is much faster than apply

--- underscore-1.8.3.js/underscore-1.8.3-analysis.js
+++ underscore-1.8.3.js/underscore-1.8.3-analysis.js
@@ -106,7 +106,8 @@
   // 二次操作返回一些回调、迭代方法
   var optimizeCb = function(func, context, argCount) {
     // 如果没有指定 this 指向，则返回原函数
-    if (context === void 0) return func;
+    if (context === void 0)
+      return func;
 
     switch (argCount == null ? 3 : argCount) {
       case 1: return function(value) {
@@ -128,6 +129,15 @@
         return func.call(context, accumulator, value, index, collection);
       };
     }
+
+    // 其实不用上面的 switch-case 语句
+    // 直接执行下面的 return 函数就行了
+    // 不这样做的原因是 call 比 apply 快很多
+    // .apply 在运行前要对作为参数的数组进行一系列检验和深拷贝，.call 则没有这些步骤
+    // 具体可以参考：
+    // https://segmentfault.com/q/1010000007894513
+    // http://www.ecma-international.org/ecma-262/5.1/#sec-15.3.4.3
+    // http://www.ecma-international.org/ecma-262/5.1/#sec-15.3.4.4
     return function() {
       return func.apply(context, arguments);
     };
